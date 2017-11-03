f5844bd1b56dac5426734751189cd23ac0128613
blueyi1103 <blueyi1103@gmail.com>
2017-11-03 15:34:26 +0800

    ...

--- httpd/index.js
+++ httpd/index.js
@@ -1,10 +1,14 @@
- 'use strict';
+'use strict';
 
  let http = require('http');
 
  http.createServer((request, response) => {
- // 傳送 HTTP header
+ request.on('end', () => {
+ console.log('Request method: ' + request.method);
+ console.log('Request url: ' + request.url);
+ });
 
+ // 傳送 HTTP header
  // HTTP Status: 200 : OK
  // Content Type: text/plain
  response.writeHead(200, {
@@ -13,9 +17,7 @@
 
  // 傳送回應內容。
  response.end('Hello World!\n');
-
- console.log('request.headers: \n', request.headers)
  }).listen(8088);
 
  // log message to Console
- console.log(' 伺服器啓動，連線 url: http://127.0.0.1:8088/');
+ console.log('Server running at http://127.0.0.1:8088/');
